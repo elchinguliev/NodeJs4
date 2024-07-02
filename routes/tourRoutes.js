122d0c754d56af8d3a11b40c25fbb7bdba80bc16
Elchin <103633223+elchinguliev@users.noreply.github.com>
2024-07-02 16:03:23 +0400

    Update tourRoutes.js

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,6 @@
 const express=require("express");
 const router = express.Router();
-
 const controller =require('./../controllers/tourController')
-
 router.route("/").get(controller.getAllTours).post(controller.createTour);
-
 router.route("/:id").get(controller.getTour).patch(controller.updateTour).delete(controller.deleteTour);
-
 module.exports=router;
