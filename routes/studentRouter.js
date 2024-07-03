7e8822df0f4c0b7241e9ee752e056b8191faabc8
Elchin <103633223+elchinguliev@users.noreply.github.com>
2024-07-03 09:02:57 +0400

    Delete routes/studentRouter.js

--- routes/studentRouter.js
+++
@@ -1,6 +0,0 @@
-const express=require("express");
-const studentController=require('./../controllers/studentController');
-const router = express.Router();
-router.param("id",studentController.checkId);
-router.route("/").get(studentController.getAllStudents).post(studentController.checkBody,studentController.createStudent);
-module.exports=router;
