bb88822437626aa1b577deb3c7bc134437f964e1
Elchin <103633223+elchinguliev@users.noreply.github.com>
2024-07-08 09:08:09 +0400

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -3,9 +3,4 @@ const studentController=require('./../controllers/studentController');
 const router = express.Router();
 router.param("id",studentController.checkId);
 router.route("/").get(studentController.getAllStudents).post(studentController.checkBody,studentController.createStudent);
-router
-  .route("/:id")
-  .get(studentController.checkId,studentController.getStudent)
-  .patch(studentController.checkId,studentController.checkBody,studentController.updateStudent)
-  .delete(studentController.checkId,studentController.deleteStudent);
 module.exports=router;
